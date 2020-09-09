1809327c82ab691c2d22eb782adaeb38dd8d448c
doeun-kim <doeun2@naver.com>
2020-09-09 13:34:52 +0900

    make it simple using java

---
+++ _includes/google.js
@@ -0,0 +1,9 @@
+document.write( '    <!-- Global site tag (gtag.js) - Google Analytics -->\n' );
+document.write( '<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-159387100-1\"></script>\n' );
+document.write( '<script>\n' );
+document.write( '  window.dataLayer = window.dataLayer || [];\n' );
+document.write( '  function gtag(){dataLayer.push(arguments);}\n' );
+document.write( '  gtag(\'js\', new Date());\n' );
+document.write( '\n' );
+document.write( '  gtag(\'config\', \'UA-159387100-1\');\n' );
+document.write( '</script>' );
